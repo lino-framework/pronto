7091a3e1a5e5bd2a764ec44859336fc561025690
Luc Saffre <luc.saffre@gmail.com>
2020-07-14 12:07:29 +0300

    http://luc.lino-framework.org/blog/2020/0714.html

--- lino_pronto/__init__.py
+++ lino_pronto/__init__.py
@@ -11,10 +11,8 @@
 
 """
 
-import os
+from .setup_info import SETUP_INFO
 
-fn = os.path.join(os.path.dirname(__file__), 'setup_info.py')
-exec(compile(open(fn, "rb").read(), fn, 'exec'))
 __version__ = SETUP_INFO['version']
 
 intersphinx_urls = dict(docs="http://pronto.lino-framework.org")
