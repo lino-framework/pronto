59d59a32f5c93567e9851d7b1e600ac60c15fb5c
Luc Saffre <luc.saffre@gmail.com>
2021-04-25 19:10:32 +0300

    fix redirected urls

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ ns = setup_from_tasks(
     globals(), "lino_pronto",
     languages="en de fr et nl".split(),
     # tolerate_sphinx_warnings=True,
-    blogref_url="http://luc.lino-framework.org",
+    blogref_url="https://luc.lino-framework.org",
     locale_dir='lino_pronto/lib/pronto/locale',
     revision_control_system='git',
     cleanable_files=['docs/api/lino_pronto.*'],
